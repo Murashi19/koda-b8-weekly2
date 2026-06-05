4765409a098a41cdfbfc43e87cdf1a8312d4e8ec
MuhamadRafliShidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-06-05 17:34:43 +0700

    refactor: remove unused html and css code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 import { updateUser } from "./modules/user.js";
-import { createCartBadge } from "./modules/cart.js";
-import { initSearch } from "./modules/search.js";
 import { renderCategories } from "./modules/category.js";
 import { renderNavbar } from "./modules/navbar.js";
 
@@ -8,9 +6,7 @@ import { navbarItems } from "./data/navbar.js";
 import { categories } from "./data/categories.js";
 
 updateUser("Rafli");
-createCartBadge(3);
 
-initSearch();
 
 renderNavbar(navbarItems)
 renderCategories(categories);
